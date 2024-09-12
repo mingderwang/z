87b350ba550bb5ba2675fe3073071f46421ad49f
mingderwang <mingderwang@me.com>
2024-09-12 08:21:29 -0700

    🐮🥩

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from './main.ts'
-const server = Bun.serve({
-  port: 3001,
-  async fetch(request: Request, server: Server) {
-  return app.handle(request)
-  }
-});
-
-console.log(`🐯Listening on http://localhost:${server.port}`);
